5beb927b9b12f26059aa025ad59df5568c4da4a9
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-01-17 10:43:20 +0700

    FEAT: setting tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        "kl-blue": "#4678F3",
+        "kl-blue-light": "#5CA6F7",
+        "kl-gray": "#808080",
+      },
+    },
   },
   plugins: [],
-}
+};
