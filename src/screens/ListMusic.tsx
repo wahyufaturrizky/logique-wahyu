19291e93bc5332ef6f003c743c9673e5f09993a0
wahyufaturrizki <wahyufaturrizkyy@gmail.com>
2024-01-17 16:23:33 +0700

    FEAT: modal search

--- src/screens/ListMusic.tsx
+++ src/screens/ListMusic.tsx
@@ -1,6 +1,7 @@
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import NgMenuIcon from "../assets/image/menu.png";
 import NgLogo from "../assets/image/ngmusic.png";
+import CloseIcon from "../assets/image/close.png";
 import NgSearchIcon from "../assets/image/search.png";
 import Image from "../components/Image";
 import Text from "../components/Text";
@@ -9,13 +10,24 @@ import { useListMusics } from "../services/musics/useMusic";
 import "./ListMusic.css";
 import Card from "../components/Card";
 import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import SearchInput from "../components/SearchInput";
+import Button from "../components/Button";
+import { watch } from "fs";
 
 function ListMusic() {
+  const [isSearch, setIsSearch] = useState(false);
+
   const { state } = useLocation();
 
   const { search } = state;
 
-  const { watch: watchSearch } = useForm({
+  const {
+    watch: watchSearch,
+    control,
+    setValue,
+    getValues,
+  } = useForm({
     defaultValues: {
       search: search,
     },
@@ -36,42 +48,82 @@ function ListMusic() {
   });
 
   return (
-    <div className="container">
-      <div className="header">
-        <div className="flex justify-between p-20">
-          <Image src={NgMenuIcon} alt="ng-music-logo" />
-          <Image src={NgLogo} alt="ng-music-logo" />
-          <Image src={NgSearchIcon} alt="ng-music-logo" />
-        </div>
+    <div>
+      <div className="container">
+        <div className="header">
+          <div className="flex justify-between p-20">
+            <Image src={NgMenuIcon} alt="ng-music-logo" />
+            <Image src={NgLogo} alt="ng-music-logo" />
+            <Image onClick={() => setIsSearch(true)} src={NgSearchIcon} alt="ng-music-logo" />
+          </div>
 
-        <div className="px-7 py-4 flex justify-center items-center">
-          <Text label="Search result for : " className="font-roboto text-sm text-[#334155]" />
+          <div className="px-7 py-4 flex justify-center items-center">
+            <Text label="Search result for : " className="font-roboto text-sm text-[#334155]" />
 
-          <Text
-            label={watchSearch("search")}
-            className="font-roboto text-lg font-bold text-ng-burple ml-2"
-          />
-        </div>
-
-        <div className="px-7 py-0">
-          {isPendingMusics ? (
             <Text
-              label="Loading..."
-              className="font-roboto font-normal text-center text-[#334155]"
+              label={getValues("search")}
+              className="font-roboto text-lg font-bold text-ng-burple ml-2"
             />
-          ) : dataMusics instanceof Array ? (
-            dataMusics?.map((data: any, index: number) => {
-              return (
-                <Card
-                  onClick={() => window.open(data.trackViewUrl, "_blank")}
-                  key={index}
-                  {...data}
-                />
-              );
-            })
-          ) : null}
+          </div>
+
+          <div className="px-7 py-0">
+            {isPendingMusics ? (
+              <Text
+                label="Loading..."
+                className="font-roboto font-normal text-center text-[#334155]"
+              />
+            ) : dataMusics instanceof Array ? (
+              dataMusics?.map((data: any, index: number) => {
+                return (
+                  <Card
+                    onClick={() => window.open(data.trackViewUrl, "_blank")}
+                    key={index}
+                    {...data}
+                  />
+                );
+              })
+            ) : null}
+          </div>
         </div>
       </div>
+
+      {isSearch && (
+        <div className="bg-black/50 p-10 absolute left-0 top-0 right-0 bottom-0">
+          <div className="flex justify-end mb-60">
+            <Image onClick={() => setIsSearch(false)} src={CloseIcon} alt="ng-music-logo" />
+          </div>
+
+          <div>
+            <Controller
+              control={control}
+              name="search"
+              render={({ field: { onChange, onBlur, value } }) => (
+                <SearchInput
+                  onChange={onChange}
+                  placeholder="Cari"
+                  onBlur={onBlur}
+                  value={value}
+                  name="search"
+                  type="text"
+                  required
+                  showIcon={false}
+                  className="w-full p-4 ps-10 text-sm text-ng-blue-gray text-center rounded-full bg-white"
+                />
+              )}
+            />
+
+            <Button
+              label="Search"
+              onClick={() => {
+                setValue("search", watchSearch("search"));
+                setIsSearch(false);
+              }}
+              type="button"
+              className="flex items-center w-full mt-4 justify-center rounded-full bg-ng-burple px-3 py-3 text-sm font-semibold leading-6 text-white shadow-sm"
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
